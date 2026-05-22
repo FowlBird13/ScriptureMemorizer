66e155cad6bde80dad1ef41fb0c393704265d5b1
Garyeong Bae <orisumin@gmail.com>
2026-05-22 15:27:42 -0600

    added attributes and setters

--- Reference.cs
+++ Reference.cs
@@ -2,5 +2,22 @@ using System;
 
 public class Reference
 {
-    
+    private string _GBbook;
+    private int _GBchapter;
+    private int _GBverse;
+    private int _GBEndVerse;
+
+    public void SetBook(string book)
+    {
+        _GBbook = book;
+    }
+    public void SetChapter(int chapter)
+    {
+        _GBchapter = chapter;
+    }
+    public void SetVerse(int verse)
+    {
+        _GBverse = verse;
+    }
+
 }
