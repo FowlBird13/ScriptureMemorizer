c9d9c03e0606db55c09c5e51089d0d603c55b1a5
FowlBird13 <bradleympeterson05@gmail.com>
2026-05-26 13:28:42 -0600

    added comments to word class

--- Word.cs
+++ Word.cs
@@ -1,5 +1,8 @@
 using System;
 
+/// <summary>
+/// Class containing a single word that can be set to Hidden or Shown.
+/// </summary>
 public class Word
 {
     private string _bmpWord;
@@ -11,16 +14,27 @@ public class Word
         _bmpHidden = false;
     }
 
+    /// <summary>
+    /// Set the status of the word to Hidden
+    /// </summary>
     public void Hide()
     {
         _bmpHidden = true;
     }
 
+    /// <summary>
+    /// Set the statuse of the word to Show
+    /// </summary>
     public void Show()
     {
         _bmpHidden = false;
     }
 
+    /// <summary>
+    /// Return the word string. If the word is Hidden, return a string of underscores
+    /// as long as the word string instead.
+    /// </summary>
+    /// <returns>string</returns>
     public string ToFormattedString()
     {
         string bmpOutput = "";
