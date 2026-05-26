ca495984733d97122962f85fb82964feb71d0391
Garyeong Bae <orisumin@gmail.com>
2026-05-26 12:33:50 -0600

    added ToDisplayFormat function

--- Scripture.cs
+++ Scripture.cs
@@ -42,27 +42,5 @@ public class Scripture
         }
         return formattedScirpture;
     }
-    public void LoadFile(string fileName)
-    {
-        string[] scriptureList = System.IO.File.ReadAllLines(fileName);
-        string rawReference;
-        string rawWords;
-        foreach(string rawScripture in scriptureList)
-        {
-            rawReference = rawScripture.Split("|")[0];
-            rawWords = rawScripture.Split("|")[1];
-        }
-    }
-    public Reference RawToReference(string rawReference)
-    {
-        Reference newRef = new Reference(); 
-        newRef.SetBook(rawReference.Split(",")[0]);
-        newRef.SetChapter(int.Parse(rawReference.Split(",")[1]);
-        newRef.SetVerse(int.Parse(rawReference.Split(",")[2]);
-        return newRef;
-    }
-    public List<Word> RawToWords(string rawWords)
-    {
-        
-    }
+
 }
