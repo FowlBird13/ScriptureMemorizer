f51af9b128514a632ae97b2e8dbd353411490307
Garyeong Bae <orisumin@gmail.com>
2026-05-26 11:26:21 -0600

    edited the constructors

--- Scripture.cs
+++ Scripture.cs
@@ -8,8 +8,10 @@ public class Scripture
 
     public Scripture()
     {
-        
-
+        SetWords([]);
+        _GBreference.SetBook("None selected");
+        _GBreference.SetChapter(0);
+        _GBreference.SetVerse(0);
     }
     public Scripture(Reference reference, List<Word> words)
     {
